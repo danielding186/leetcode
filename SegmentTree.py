9c84dd742b2587f41d530ac4a254994015e1eef4
Yanjing Ding <dding@wyze.com>
2019-11-08 16:21:11 -0800

    Binary Tree and Segment Tree

--- SegmentTree.py
+++ SegmentTree.py
@@ -9,22 +9,22 @@ class SegmentTree(object):
         
     def buildTree(self, i, start, end, arr):    
         if start > end:
-            return float('inf')
+            return -1
         
         if start == end:
             self.lo[i] = start
             self.hi[i] = end
-            self.min[i] = arr[start]
+            self.min[i] = start
             return self.min[i]
         
         mid = (start + end) // 2
-        
-        leftMin = self.buildTree(i*2+1, start, mid, arr)
-        rightMin = self.buildTree(i*2+2, mid+1, end, arr)
-        
         self.lo[i] = start
         self.hi[i] = end
-        self.min[i] = min(leftMin, rightMin)
+        
+        left = self.buildTree(i*2+1, start, mid, arr)
+        right = self.buildTree(i*2+2, mid+1, end, arr)
+        self.min[i] = left if self.arr[left] < self.arr[right] else right
+        
         return self.min[i]
     
     def query(self, start, end):
@@ -32,30 +32,30 @@ class SegmentTree(object):
         
     def __query__(self, i, start, end):
         if start > end or self.lo[i] > end or self.hi[i] < start:
-            return float('inf')
+            return -1
         
         if self.lo[i] >= start and self.hi[i] <= end:
             return self.min[i]
+
+        left = self.__query__(i*2+1, start, end)
+        right = self.__query__(i*2+2, start, end)
         
-        return min(self.__query__(i*2+1, start, end), self.__query__(i*2+2, start, end))
+        if left == -1: return right
+        if right == -1: return left
+
+        return left if self.arr[left] < self.arr[right] else right
+
 
 if __name__ == '__main__':
     arr = [1, 7, 3, 2, 9]
     sgTree = SegmentTree(arr)
     
-    assert(sgTree.query(0, 4) == 1)
-    assert(sgTree.query(1, 4) == 2)
-    assert(sgTree.query(2, 4) == 2)
-    assert(sgTree.query(3, 4) == 2)
-    assert(sgTree.query(4, 4) == 9)
-    assert(sgTree.query(1, 2) == 3)
-    assert(sgTree.query(3, 4) == 2)
+    assert(sgTree.query(0, 4) == 0)
+    assert(sgTree.query(1, 4) == 3)
+    assert(sgTree.query(2, 4) == 3)
+    assert(sgTree.query(3, 4) == 3)
+    assert(sgTree.query(4, 4) == 4)
+    assert(sgTree.query(1, 2) == 2)
+    assert(sgTree.query(3, 4) == 3)
 
-    for N in range(10, 1023, 13):
-        arr = range(0, N)
-        sgTree = SegmentTree(arr)
-        for i in range(1, N):
-            assert(sgTree.query(0, i) == 0)
-            assert(sgTree.query(i, N) == i)
-            assert(sgTree.query(i, i+1) == i)
-            assert(sgTree.query(N//2, N) == N//2)
+    
